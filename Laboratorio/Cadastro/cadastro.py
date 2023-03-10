f1174834e33d42b67192bd8c0cdbb5f3b87022ff
Ana Júlia da Rosa <56845067+anajur@users.noreply.github.com>
2023-03-09 22:48:52 -0300

    Create cadastro.py
    cadastro

---
+++ Laboratorio/Cadastro/cadastro.py
@@ -0,0 +1,12 @@
+altura = float(input("Digite sua altura: "))
+
+print("Por favor, digite seu endereço")
+
+cep = int(input("CEP: "))
+rua = input("Rua: ")
+idade = int(input("Número: "))
+bairro = input("Bairro: ")
+complemento = input("Complemento: ")
+cidade = input("Cidade: ")
+estado = input("Estado: ")
+pais = input("País: ")
