a88b61508fd56755d2b01f6fa28c2ca5d2ecf4a7
Ana Júlia da Rosa <56845067+anajur@users.noreply.github.com>
2023-03-09 22:32:41 -0300

    Create school.py
    a system for calculating the average of grades

---
+++ Laboratorio/School/school.py
@@ -0,0 +1,8 @@
+print("Welcome to Ana's school")
+
+first_grade = float(input("First grade: "))
+second_grade = float(input("Second grade: "))
+third_grade = float(input("Third grade: "))
+final_grade = (first_grade + second_grade + third_grade) / 3
+
+print("Your final grade is {:.1f}".format(final_grade))
